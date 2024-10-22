f3e622818cf4daebd8acc183aa8154d11421425f
jiruhameru <ZAlHamer@gmail.com>
2024-10-23 00:01:31 +0900

    Added link to Browse button

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default async function HomePage() {
             (Artificial Intelligence) across various nations.
           </p>
           <div className="space-x-4">
-            <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
+            <Link href="#principles" className={cn(buttonVariants({ size: "lg" }))}>
               Browse
             </Link>
             <Link
